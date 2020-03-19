30e2ffe85a8bc02656967dc99a3f005d5e854561
Maryna <maksimarina@gmail.com>
2020-03-18 20:32:01 -0400

    IQ1

--- src/com/syntax/class13/Q1.java
+++ src/com/syntax/class13/Q1.java
@@ -3,8 +3,23 @@ package com.syntax.class13;
 public class Q1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// write a program to swap two numbers without a temp. variable
+		//swap two strings
+		int a = 10;
+		int b= 20;
+		a=a+b;// 30
+		b=a-b;// 30-20=10
+		a=a-b;
+		System.out.println("the value of a is " +a);
+		System.out.println("the value of b is "+ b);
+		
+		String str1="Day";
+		String str2 = "Night";
+		str1=str1+str2; //DayNight
+		str2=str1.substring(0 , str1.length()-str2.length()); //day
+		str1=str1.substring(str2.length()); //night
+		System.out.println(str1);
+		System.out.println(str2);
 	}
 
 }
